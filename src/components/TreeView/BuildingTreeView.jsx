71c1db7bcc4ebe1fdac44eaccf3eb4e357e4d564
SeyyedMohammadAli Abyar <seyyed.abyar@gmail.com>
2024-08-10 02:22:40 +0330

    add TreeView Code

---
+++ src/components/TreeView/BuildingTreeView.jsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView';
+import { TreeItem } from '@mui/x-tree-view/TreeItem';
+import MailIcon from '@mui/icons-material/Mail';
+import './BuildingTreeView.css';
+
+const data = {
+  id: 'root',
+  title: 'پروژه ها',
+  children: [
+    {
+      id: '65cf312d52c2c25b64f56910',
+      title: 'ساختمان',
+      children: [
+        {
+          id: '65cf317a52c2c25b64f5691c',
+          title: 'وب اپلیکیشن',
+          children: []
+        },
+        {
+          id: '65cf318f52c2c25b64f56920',
+          title: 'حل مسائل',
+          children: [
+            {
+              id: '65cf31b652c2c25b64f56924',
+              title: 'مسئله شماره یک و دو',
+              children: []
+            }
+          ]
+        }
+      ]
+    },
+    {
+      id: '65cf315252c2c25b64f56914',
+      title: 'به سوی ظهور',
+      children: [
+        {
+          id: '65cf31df52c2c25b64f56928',
+          title: 'آخرالزمان',
+          children: []
+        }
+      ]
+    },
+    {
+      id: '65cf320f52c2c25b64f5692c',
+      title: 'بدون زیر پروژه',
+      children: []
+    }
+  ],
+};
+
+// تابع برای رندر کردن درخت به طور بازگشتی
+const renderTreeItems = (node) => {
+  return (
+    <TreeItem
+      key={node.id}
+      itemId={node.id}
+      label={node.title}
+      icon={MailIcon} // می‌توانید آیکون دیگری استفاده کنید
+    >
+      {node.children && node.children.length > 0 && (
+        <>
+          {node.children.map(child => renderTreeItems(child))}
+        </>
+      )}
+    </TreeItem>
+  );
+};
+
+const BuildingTreeView = () => {
+  const [selectedItemId, setSelectedItemId] = React.useState(null);
+  const [parentItemId, setParentItemId] = React.useState(null);
+
+  const handleItemClick = (itemId, parentId) => {
+    setSelectedItemId(itemId);
+    setParentItemId(parentId);
+  };
+
+  return (
+    <div className='TreeBox'>
+      <SimpleTreeView>
+        {renderTreeItems(data)}
+      </SimpleTreeView>
+    </div>
+  );
+};
+
+export default BuildingTreeView;
